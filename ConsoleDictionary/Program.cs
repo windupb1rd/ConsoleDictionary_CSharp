73f2be4e6bf54df409dbcf76b455a7df0cd5e4a4
windupb1rd <windupb1rd@WINDUPB1RD-WIND>
2022-05-25 15:06:24 +0900

    Add project files.

---
+++ ConsoleDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object searchQuery = FreeDictionaryAPI.GetWordsInJson(Console.ReadLine());
+            Console.WriteLine(searchQuery);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleDictionary/api.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleDictionary
+{
+    public class FreeDictionaryAPI
+    {
+        public class JSONModelCollection
+        {
+            public List<JSONModel> Jsonmodel { get; set; }
+        }
+        public class JSONModel
+        {   
+            public string word;
+            public string phonetic;
+        }
+        public static object GetWordsInJson(string query)
+        {
+            WebClient client = new();
+            string replyFromApi = client.DownloadString("https://api.dictionaryapi.dev/api/v2/entries/en/" + query);
+
+            //var listOfDefinitions = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(replyFromApi);
+            //string entry1 = JsonConvert.SerializeObject(listOfDefinitions[1]);
+
+            var final = JsonConvert.DeserializeObject<JSONModelCollection>(replyFromApi);
+
+            return final;
+        }
+
+    }
+}
